1b2bf08abc685cbf416c076ad6c3b70ffb4b5291
palmigudmunds <palmig20@ru.is>
2023-01-25 20:17:03 +0000

    Fixing typos in text from Jökul&Co

--- src/components/PricesItems/index.js
+++ src/components/PricesItems/index.js
@@ -14,27 +14,27 @@ const PricesItems = () => {
                         <ul className='leaders space-y-10'>
                             <li>
                                 <span>{t('prices-suits')}</span>
-                                <span>{t('prices-from')} 79.980,-kr.</span>
+                                <span>{t('prices-from')} 69.980,-</span>
                             </li>
                             <li>
                                 <span>{t('prices-shirts')}</span>
-                                <span>{t('prices-from')} 14.980,-kr.</span>
+                                <span>{t('prices-from')} 14.980,-</span>
                             </li>
                             <li>
                                 <span>{t('prices-angora')}</span>
-                                <span>99.980,-kr.</span>
+                                <span>99.980,-</span>
                             </li>
                             <li>
                                 <span>{t('prices-cashmere')}</span>
-                                <span>129.980,-kr.</span>
+                                <span>129.980,-</span>
                             </li>
                             <li>
                                 <span>{t('prices-afiogeg-cufflinks')}</span>
-                                <span>5.490,-kr.</span>
+                                <span>5.490,-</span>
                             </li>
                             <li>
                                 <span>{t('prices-afiogeg-tieclip')}</span>
-                                <span>12.490,-kr.</span>
+                                <span>12.490,-</span>
                             </li>
                         </ul>
                     </div>
