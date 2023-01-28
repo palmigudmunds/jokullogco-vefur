ffb877aa36b7927d3ef87d447f06efcacf49c84e
palmigudmunds <palmig20@ru.is>
2023-01-28 12:14:11 +0000

    Added animations to landing page using framer-motion

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import umokkur from '../../resources/images/umokkur.jpg';
+
+const paragraphVariants = {
+    offscreen: {
+      x: 200,
+      opacity: 0
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 2
+      }
+    }
+};
+
+const headerVariants = {
+    offscreen: {
+      x: 100,
+      opacity: 0
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 2
+      }
+    }
+};
 
 const AboutUs = () => {
 
     const { t } = useTranslation();
 
     let navigate = useNavigate();
+
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+          control.start("onscreen");
+        } 
+    }, [control, inView]);
     
     return (
         <div id="um-okkur" className="flex flex-col-reverse md:flex-row justify-center max-w-none">
@@ -15,10 +57,24 @@ const AboutUs = () => {
                 <img className="w-full h-auto object-cover md:max-w-[30rem] max-w-[24rem] mb-5" src={umokkur} alt="About"/>
             </div>
             <div className="md:w-1/2 flex flex-col items-start md:items-center">
-                <h2 className="text-start md:w-10/12 lg:w-8/12">{t('about-title')}</h2>
-                <p className="pt-5 md:w-10/12 lg:w-8/12">
+                <motion.h2 
+                className="text-start md:w-10/12 lg:w-8/12"
+                ref={ref}
+                variants={headerVariants}
+                initial="offscreen"
+                animate={control}
+                >
+                    {t('about-title')}
+                </motion.h2>
+                <motion.p 
+                className="pt-5 md:w-10/12 lg:w-8/12"
+                ref={ref}
+                variants={paragraphVariants}
+                initial="offscreen"
+                animate={control}
+                >
                     {t('about-text')}
-                </p>
+                </motion.p>
                 <div className="mt-5 md:mt-10 flex items-start">
                     <button
                         className="bg-maingold text-white text-lg md:text-xl py-2 px-10 border border-white hover:bg-white hover:text-maingold hover:border-maingold ease-in-out duration-200"
--- src/components/TailoringInfo/index.js
+++ src/components/TailoringInfo/index.js
@@ -1,16 +1,73 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import sersaumur from '../../resources/images/sersaumur.jpg'
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import sersaumur from '../../resources/images/sersaumur.jpg';
+
+
+const paragraphVariants = {
+    offscreen: {
+      x: -200,
+      opacity: 0
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 2
+      }
+    }
+};
+
+const headerVariants = {
+    offscreen: {
+      x: -100,
+      opacity: 0
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 2
+      }
+    }
+};
 
 const TailoringInfo = () => {
 
     const { t } = useTranslation();
+
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+          control.start("onscreen");
+        } 
+    }, [control, inView]);
     
     return (
         <div id="sersaumur" className="flex flex-col md:flex-row justify-center max-w-none">
             <div className="md:w-1/2 flex flex-col items-center">
-                <h2 className="text-start md:text-start md:w-10/12 lg:w-8/12">{t('tailor-info-title')}</h2>
-                <p className="pt-5 md:w-10/12 lg:w-8/12">
+                <motion.h2 
+                className="text-start md:text-start md:w-10/12 lg:w-8/12"
+                ref={ref}
+                variants={headerVariants}
+                initial="offscreen"
+                animate={control}
+                >
+                    {t('tailor-info-title')}
+                </motion.h2>
+                <motion.p
+                className="pt-5 md:w-10/12 lg:w-8/12"
+                ref={ref}
+                variants={paragraphVariants}
+                initial="offscreen"
+                animate={control}
+                >
                     {t('tailor-info-text-1')}
                     <br></br><br></br>
                     {t('tailor-info-text-2')}
@@ -22,7 +79,7 @@ const TailoringInfo = () => {
                     {t('tailor-info-text-5')}
                     <br></br><br></br>
                     {t('tailor-info-text-6')}
-                </p>
+                </motion.p>
             </div>
             <div className="flex flex-col items-center md:w-1/2 pt-8 md:pt-0">
                 <img className="w-full h-auto object-cover md:max-w-[30rem] max-w-[24rem]" src={sersaumur} alt="Materials"/>
