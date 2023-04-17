a8e360ef7c0e3e90ca46dfb33a25ab6c5882a495
palmigudmunds <palmig20@ru.is>
2023-04-17 18:12:20 +0000

    Changed link in hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -95,7 +95,7 @@ const Hero = () => {
                         animate={control}>
                             <a
                             className="bg-maingold text-white text-lg md:text-xl py-3 px-10 border border-white hover:bg-white hover:text-maingold hover:border-maingold ease-in-out duration-200"
-                            href="https://noona.is/jokullco/book/xZQk5WWosQnFyxAhX/timeslots">{t('order-button')}</a>
+                            href="https://noona.is/jokullogco/book">{t('order-button')}</a>
                         </motion.div>
                     </div>
                 </section>
