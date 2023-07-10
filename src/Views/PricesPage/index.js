2632dbed50d3064060a46bf449498f9b3c42f026
palmigudmunds <palmig20@ru.is>
2023-07-10 21:46:58 +0000

    Changed prices and added new video to home screen

--- src/Views/PricesPage/index.js
+++ src/Views/PricesPage/index.js
@@ -1,24 +1,112 @@
-import Container from '../../components/container';
-import Loading from '../../components/Loading';
-import { Suspense } from 'react';
-import PricesHeader from '../../components/PricesHeader';
-import PricesItems from '../../components/PricesItems';
-
+import Loading from "../../components/Loading";
+import { Suspense } from "react";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 const PricesPageView = () => {
+  const { t } = useTranslation();
 
-	return (
-		<>
-			<Suspense fallback={<Loading/>}>
-                <div className='space-y-12 md:space-y-24 mb-28'>
-                    <PricesHeader />
-					<Container>
-						<PricesItems />
-					</Container>
-                </div>
-			</Suspense>
-		</>
-	);
-}
+  return (
+    <>
+      <Suspense fallback={<Loading />}>
+        <section className="max-w-screen-md mx-auto px-3 space-y-16 mb-28">
+          <section
+            id="prices-header"
+            className="relative -mt-5 flex flex-col items-center justify-center text-center text-white py-0 px-3"
+          >
+            <div className="space-y-2 flex flex-col justify-start items-center max-w-none md:max-w-screen-xl mx-auto md:max-h-screen space-x-0 px-3">
+              <div className="w-full mt-5 md:mt-0">
+                <h1 className="text-center">{t("prices-title")}</h1>
+              </div>
+            </div>
+          </section>
+          <div>
+            <h2 className="text-start md:text-start">Flíkur</h2>
+            <div className="mt-5">
+              <ul className="leaders space-y-7">
+                <li>
+                  <span>{t("prices-suits-double")}</span>
+                  <span>{t("prices-from")} 79.980,-</span>
+                </li>
+                <li>
+                  <span>{t("prices-suits-triple")}</span>
+                  <span>{t("prices-from")} 94.980,-</span>
+                </li>
+                <li>
+                  <span>{t("prices-blazer")}</span>
+                  <span>{t("prices-from")} 54.980,-</span>
+                </li>
+                <li>
+                  <span>{t("prices-pants")}</span>
+                  <span>{t("prices-from")} 27.980,-</span>
+                </li>
+                <li>
+                  <span>{t("prices-vest")}</span>
+                  <span>{t("prices-from")} 22.980,-</span>
+                </li>
+                <li>
+                  <span>{t("prices-shirts")}</span>
+                  <span>{t("prices-from")} 14.980,-</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <h2 className="text-start md:text-start">Yfirhafnir</h2>
+            <div className="mt-5">
+              <ul className="leaders space-y-7">
+                <li>
+                  <span>{t("prices-overcoats")}</span>
+                  <span>{t("prices-from")} 99.980,-</span>
+                </li>
+                <li>
+                  <span>{t("prices-raincoats")}</span>
+                  <span>{t("prices-from")} 64.980,-</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <h2 className="text-start md:text-start">Aukahlutir</h2>
+            <div className="mt-5">
+              <ul className="leaders space-y-7">
+                <li>
+                  <span>{t("prices-italian-silk-tie")}</span>
+                  <span>
+                    <span>{t("prices-from")}</span> 14.980,-
+                  </span>
+                </li>
+                <li>
+                  <span>{t("prices-italian-silk-cloth")}</span>
+                  <span>
+                    <span>{t("prices-from")}</span> 6.490,-
+                  </span>
+                </li>
+                <li>
+                  <span>{t("prices-knitted-tie")}</span>
+                  <span>
+                    <span>{t("prices-from")}</span> 15.490,-
+                  </span>
+                </li>
+                <li>
+                  <span>{t("prices-afiogeg-cufflinks")}</span>
+                  <span>
+                    <span>{t("prices-from")}</span> 5.490,-
+                  </span>
+                </li>
+                <li>
+                  <span>{t("prices-afiogeg-tieclip")}</span>
+                  <span>
+                    <span>{t("prices-from")}</span> 12.490,-
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </Suspense>
+    </>
+  );
+};
 
 export default PricesPageView;
--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -22,10 +22,10 @@ const AboutUs = () => {
       id="um-okkur"
       className="flex flex-col md:flex-row justify-center max-w-none"
     >
-      <div className="md:w-1/2 flex flex-col items-start md:items-center md:pt-10">
-        <h2 className="text-start md:w-10/12 xl:w-8/12">{t("about-title")}</h2>
-        <p className="pt-5 md:w-10/12 xl:w-8/12">{t("about-text")}</p>
-        <div className="pt-7 flex flex items-start md:w-10/12 xl:w-8/12">
+      <div className="md:w-full flex flex-col items-start md:pt-10">
+        <h2 className="text-start">{t("about-title")}</h2>
+        <p className="pt-5 md:w-10/12">{t("about-text")}</p>
+        <div className="pt-7 flex flex items-start">
           <button
             className="bg-maingold text-white text-lg md:text-xl py-2 px-10 border border-white hover:bg-white hover:text-maingold hover:border-maingold ease-in-out duration-200"
             onClick={() => navigate("/um-okkur")}
@@ -34,7 +34,7 @@ const AboutUs = () => {
           </button>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center md:w-1/2 pt-8 md:pt-0">
+      <div className="flex flex-col items-end w-full pt-8 md:pt-0">
         <img
           className="w-full h-auto object-cover xl:max-w-[32rem] max-w-[24rem]"
           src={umokkur}
